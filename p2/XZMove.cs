87b8b397ffeb84dab3aa2d35b9cba0bade8c7133
Lucas Alonso <43813085+alu0101042305@users.noreply.github.com>
2019-10-11 19:02:41 +0100

    Add files via upload

---
+++ p2/XZMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XZMove : MonoBehaviour
+{
+    public float speed = 1;
+    public float giro = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0,
+                                Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * giro);
+    }
+}
